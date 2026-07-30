b69eb97fb3ce4cef77f4abfec89c005e3124f9a6
Sid Jain <sid_26@outlook.com>
2026-07-30 02:22:05 +0000

    chore: upgrade Next.js and React

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    useTypeScriptCli: true,
+  },
   outputFileTracingExcludes: {
     "/*": ["./next.config.ts"],
   },
